bc79aae1b2e4b3f77ae51efa3c3a7da881d2f390
Putrimaulida <2041720012@student.polinema.ac.id>
2022-02-21 18:49:27 +0700

    pwl-prak03

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [WelcomeController::class,'welcome']);
 
-Route::get('/about', [App\Http\Controllers\AboutController::class, 'about']);
+Route::prefix('prodi')->group(function () {
+    Route::get('/manajemen-informatika', [WelcomeController::class, 'd3mi']);
+    Route::get('/teknik-informatika', [WelcomeController::class, 'd4ti']);  
+});
 
-Route::get('/article/{id}', [App\Http\Controllers\ArticleController::class, 'article']);
+Route::get('/news/{kbrti}', [WelcomeController::class, 'newsti']);
+
+Route::prefix('sarana')->group(function () {
+    Route::get('/perkantoran', [WelcomeController::class, 'kantor']);
+    Route::get('/kelas', [WelcomeController::class, 'kelas']);
+    Route::get('/laboratorium', [WelcomeController::class, 'lab']);
+    Route::get('/lainnya', [WelcomeController::class, 'lain']);
+
+});
+
+Route::get('/aboutus', [WelcomeController::class,'aboutus']);
+
+Route::get('/comment/{yourname}/{yourmessages}', [WelcomeController::class, 'comment']);
