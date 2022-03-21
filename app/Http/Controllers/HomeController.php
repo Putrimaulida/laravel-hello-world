a5168ae0355450ec66a5c7f760e836901abaa364
Putrimaulida <2041720012@student.polinema.ac.id>
2022-03-22 00:36:36 +0700

    Praktikum 2: Add login as Administrator

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('Home', ['homes' => Home::index()]); 
+        return view('home');
     }
 }
